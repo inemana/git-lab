5aa82a6d39b496ba5f7578d8ea5a9f5ceac77869
Inemesit Anako <inemesit.anako@bison.howard.edu>
2026-04-28 01:30:25 +0000

    update cipher code

--- mycipher.py
+++ mycipher.py
@@ -1,1 +1,29 @@
+import sys
 
+shift = int(sys.argv[1])%26
+
+text = sys.stdin.read().upper()
+
+result=""
+
+for ch in text:
+    if 'A' <= ch <= 'Z':
+        result += chr((ord(ch) - ord('A') + shift) % 26 +ord('A'))
+
+count = 0
+blocks = 0
+
+for ch in result:
+    print(ch, end = "")
+    count += 1
+
+    if count ==5:
+        print(" ", end="")
+        count = 0
+        blocks += 1
+
+    if blocks == 10:
+        print()
+        blocks = 0
+
+print()
